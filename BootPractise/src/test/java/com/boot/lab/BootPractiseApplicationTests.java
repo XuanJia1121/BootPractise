e61516651a1cb5b006d647a2c280a3e2767a29e6
IEAD\2008017 <xuanjia961121@gmail.com>
2022-08-25 17:17:47 +0800

    first

---
+++ BootPractise/src/test/java/com/boot/lab/BootPractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootPractiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
